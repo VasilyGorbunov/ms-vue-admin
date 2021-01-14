1dc023faa57ac16dd8b89f050012c9f34dbec0d5
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-01-14 18:22:13 +0300

    045 Deleting User

--- src/secure/users/Users.vue
+++ src/secure/users/Users.vue
@@ -17,7 +17,12 @@
         <td>{{user.first_name}} {{user.last_name}}</td>
         <td>{{user.email}}</td>
         <td>{{user.role.name}}</td>
-        <td></td>
+        <td>
+            <div class="btn-group mr-2">
+                <a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary">Edit</a>
+                <a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary" @click="del(user.id)">Delete</a>
+            </div>
+        </td>
       </tr>
       </tbody>
     </table>
@@ -64,12 +69,20 @@ export default {
             await load()
         }
 
+        const del = async (id: number) => {
+            if(confirm('Are you sure you want to delete this record?')) {
+                await axios.delete(`users/${id}`)
+                users.value = users.value.filter((u:any) => u.id !== id)
+            }
+        }
+
         onMounted(load)
 
         return {
             users,
             next,
-            prev
+            prev,
+            del
         }
     }
 }
