7cb42218f491b205ef53330afcf7fbb1727c6dba
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-01-20 15:06:22 +0300

    061 Chart

--- src/secure/dashboard/Dasboard.vue
+++ src/secure/dashboard/Dasboard.vue
@@ -1,9 +1,50 @@
 <template>
-  Dashboard
+  <h2>Daily Sales</h2>
+  <div id="chart"></div>
 </template>
 
 <script>
+import { onMounted } from 'vue'
+import * as c3 from 'c3'
+import axios from 'axios'
+
   export default {
-    name: "Dasboard"
+    name: "Dasboard",
+    setup() {
+      onMounted(async () => {
+        const chart = c3.generate({
+          bindto: '#chart',
+          data: {
+            x: 'x',
+            columns: [
+              ['x'],
+              ['Sales']
+            ],
+            types: {
+              Sales: 'bar'
+            }
+          },
+          axis: {
+            x: {
+              type: 'timeseries',
+              tick: {
+                format: '%Y-%m-%d'
+              }
+            }
+          }
+        });
+
+        const response = await axios.get('chart')
+        const records = response.data.data
+
+        chart.load({
+          columns: [
+            ['x', ...records.map(r => r.date)],
+            ['Sales', ...records.map(r => r.sum)]
+          ]
+        })
+
+      })
+    }
   }
 </script>
