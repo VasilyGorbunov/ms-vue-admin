8485ae80bc99ccdabec4d523e3da215fafb53be8
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-01-25 15:18:45 +0300

    067 Permissions

--- src/secure/components/Menu.vue
+++ src/secure/components/Menu.vue
@@ -5,16 +5,16 @@
         <li class="nav-item">
           <router-link active-class="active" class="nav-link" to="/dashboard">Dashboard</router-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" v-if="user.canView('users')">
           <router-link active-class="active" class="nav-link" to="/users">Users</router-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" v-if="user.canView('roles')">
           <router-link active-class="active" class="nav-link" to="/roles">Roles</router-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" v-if="user.canView('products')">
           <router-link active-class="active" class="nav-link" to="/products">Products</router-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" v-if="user.canView('orders')">
           <router-link active-class="active" class="nav-link" to="/orders">Orders</router-link>
         </li>
       </ul>
@@ -22,8 +22,18 @@
   </nav>
 </template>
 
-<script>
+<script lang="ts">
+import { computed } from 'vue'
+import { useStore } from 'vuex'
   export default {
-    name: "Menu"
+    name: "Menu",
+    setup() {
+      const store = useStore()
+      const user = computed(() => store.state.User.user)
+
+      return {
+        user
+      }
+    }
   }
 </script>
--- src/secure/users/Users.vue
+++ src/secure/users/Users.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="d-flex justify-content-between flex-wrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <div class="btn-toolbar mb-2 mb-md-0">
+        <div class="btn-toolbar mb-2 mb-md-0" v-if="authUser.canEdit('users')">
             <router-link to="/users/create" class="btn btn-sm btn-outline-secondary">Add</router-link>
         </div>
     </div>
@@ -22,7 +22,7 @@
         <td>{{user.email}}</td>
         <td>{{user.role.name}}</td>
         <td>
-            <div class="btn-group mr-2">
+            <div class="btn-group mr-2" v-if="authUser.canEdit('users')">
                 <router-link :to="`/users/${user.id}/edit`" class="btn btn-sm btn-outline-secondary">Edit</router-link>
                 <a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary" @click="del(user.id)">Delete</a>
             </div>
@@ -36,18 +36,22 @@
 
 <script lang="ts">
 
-import {ref, onMounted} from 'vue'
+import {ref, onMounted, computed} from 'vue'
 import axios from 'axios'
 // import {User} from '@/classes/user'
 import { Entity } from '@/interfaces/entity'
 import Paginator from '../components/Paginator.vue'
+import { useStore } from 'vuex'
 
 export default {
   components: { Paginator },
     name: 'Users',
     setup() {
+        const store = useStore()
         const users = ref([])
         const lastPage = ref(0)
+
+        const authUser = computed(() => store.state.User.user)
     
         const load = async (page: number = 1) => {
             const response = await axios.get(`users?page=${page}`)
@@ -66,6 +70,7 @@ export default {
         onMounted(load)
 
         return {
+          authUser,
             users,
             del,
             load
